9f4a836873a73c0df754fe1f7e8c810020744804
sania <saniainf@gmail.com>
2016-06-28 17:51:51 +0600

    123

---
+++ SecretPicture/SecretPicture/FormSecretPicture.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SecretPicture
+{
+    partial class FormSecretPicture
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSecretPicture));
+            this.buttonShow1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonShow1
+            // 
+            this.buttonShow1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonShow1.Location = new System.Drawing.Point(12, 12);
+            this.buttonShow1.Name = "buttonShow1";
+            this.buttonShow1.Size = new System.Drawing.Size(94, 39);
+            this.buttonShow1.TabIndex = 0;
+            this.buttonShow1.Text = "кактус";
+            this.buttonShow1.UseVisualStyleBackColor = true;
+            this.buttonShow1.Click += new System.EventHandler(this.buttonShow1_Click);
+            // 
+            // FormSecretPicture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(698, 473);
+            this.Controls.Add(this.buttonShow1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "FormSecretPicture";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Картинка с секретом";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonShow1;
+    }
+}
+
