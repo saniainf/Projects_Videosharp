f31f9639e2092dc3d7926f795ee99d26ab83d7f2
sania <saniainf@gmail.com>
2016-06-30 17:29:32 +0600

    FastClick

---
+++ FastClick/FastClick/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace FastClick
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCounter.Font = new System.Drawing.Font("Candara", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCounter.Location = new System.Drawing.Point(12, 105);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(258, 169);
+            this.lblCounter.TabIndex = 0;
+            this.lblCounter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblCounter.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblCounter_MouseDown);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Candara", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStart.Location = new System.Drawing.Point(12, 39);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(258, 46);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Начать игру";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Candara", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(12, 284);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(258, 39);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "---";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuAbout});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(282, 24);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // menuAbout
+            // 
+            this.menuAbout.Name = "menuAbout";
+            this.menuAbout.Size = new System.Drawing.Size(94, 20);
+            this.menuAbout.Text = "О программе";
+            this.menuAbout.Click += new System.EventHandler(this.menuAbout_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 345);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.menuStrip);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fast Click";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuAbout;
+    }
+}
+
---
+++ FastClick/FastClick/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastClick
+{
+    public partial class frmMain : Form
+    {
+        long timeStart;
+        long timeStop;
+        int maxCounter = 10;
+        int counter;
+        string mode = "stop";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            startGame();
+        }
+
+        private void startGame()
+        {
+            if (mode == "play")
+                return;
+            mode = "play";
+            counter = maxCounter;
+            lblCounter.Text = counter.ToString();
+            lblResult.Text = "";
+            btnStart.Enabled = false;
+        }
+
+        private void playGame()
+        {
+            if (mode == "stop")
+                return;
+            if (counter == maxCounter)
+                timeStart = DateTime.Now.Ticks;
+            if (counter > 0)
+                counter--;
+            lblCounter.Text = counter.ToString();
+            if (counter == 0)
+                stopGame();
+        }
+
+        private void stopGame()
+        {
+            mode = "stop";
+            timeStop = DateTime.Now.Ticks;
+            double seconds = TimeSpan.FromTicks(timeStop - timeStart).TotalSeconds;
+            double clicksPerSeconds = maxCounter / seconds;
+            lblResult.Text = clicksPerSeconds.ToString("#.#") + " кликов за секунду";
+            btnStart.Enabled = true;
+        }
+
+        private void lblCounter_Click(object sender, EventArgs e)
+        {
+            playGame();
+        }
+
+        private void lblCounter_MouseDown(object sender, MouseEventArgs e)
+        {
+            playGame();
+        }
+
+        private void menuAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа написана мной.\n\nИ она работает.");
+        }
+    }
+}
