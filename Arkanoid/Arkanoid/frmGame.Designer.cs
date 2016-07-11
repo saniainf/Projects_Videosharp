1bfb6c65de9d0573e1d2ee91e91c0f55bc9fabdd
sania <saniainf@gmail.com>
2016-07-11 18:30:41 +0600

    123

--- Arkanoid/Arkanoid/frmGame.Designer.cs
+++ Arkanoid/Arkanoid/frmGame.Designer.cs
@@ -30,172 +30,72 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGame));
-            this.paddle = new System.Windows.Forms.Button();
-            this.ball = new System.Windows.Forms.RadioButton();
-            this.lblBrick1 = new System.Windows.Forms.Label();
-            this.lblBrick2 = new System.Windows.Forms.Label();
-            this.lblBrick3 = new System.Windows.Forms.Label();
-            this.lblBrick4 = new System.Windows.Forms.Label();
-            this.lblBrick7 = new System.Windows.Forms.Label();
-            this.lblBrick6 = new System.Windows.Forms.Label();
-            this.lblBrick5 = new System.Windows.Forms.Label();
-            this.lblBrick8 = new System.Windows.Forms.Label();
             this.tmrBall = new System.Windows.Forms.Timer(this.components);
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.leftWall = new System.Windows.Forms.PictureBox();
+            this.TopWall = new System.Windows.Forms.PictureBox();
+            this.rightWall = new System.Windows.Forms.PictureBox();
+            this.paddle = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.leftWall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopWall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightWall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
             this.SuspendLayout();
-            // 
-            // paddle
-            // 
-            this.paddle.BackColor = System.Drawing.Color.MidnightBlue;
-            this.paddle.Enabled = false;
-            this.paddle.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.paddle.ForeColor = System.Drawing.Color.White;
-            this.paddle.Location = new System.Drawing.Point(350, 574);
-            this.paddle.Name = "paddle";
-            this.paddle.Size = new System.Drawing.Size(102, 23);
-            this.paddle.TabIndex = 1;
-            this.paddle.Text = "------------";
-            this.paddle.UseVisualStyleBackColor = false;
-            // 
-            // ball
-            // 
-            this.ball.AutoSize = true;
-            this.ball.BackColor = System.Drawing.Color.Transparent;
-            this.ball.Checked = true;
-            this.ball.Enabled = false;
-            this.ball.ForeColor = System.Drawing.Color.Transparent;
-            this.ball.Location = new System.Drawing.Point(396, 555);
-            this.ball.Name = "ball";
-            this.ball.Size = new System.Drawing.Size(14, 13);
-            this.ball.TabIndex = 2;
-            this.ball.TabStop = true;
-            this.ball.UseVisualStyleBackColor = false;
-            // 
-            // lblBrick1
-            // 
-            this.lblBrick1.BackColor = System.Drawing.Color.Firebrick;
-            this.lblBrick1.Location = new System.Drawing.Point(114, 131);
-            this.lblBrick1.Name = "lblBrick1";
-            this.lblBrick1.Size = new System.Drawing.Size(75, 23);
-            this.lblBrick1.TabIndex = 0;
-            // 
-            // lblBrick2
-            // 
-            this.lblBrick2.BackColor = System.Drawing.Color.Firebrick;
-            this.lblBrick2.Location = new System.Drawing.Point(280, 131);
-            this.lblBrick2.Name = "lblBrick2";
-            this.lblBrick2.Size = new System.Drawing.Size(75, 23);
-            this.lblBrick2.TabIndex = 0;
-            // 
-            // lblBrick3
-            // 
-            this.lblBrick3.BackColor = System.Drawing.Color.Firebrick;
-            this.lblBrick3.Location = new System.Drawing.Point(446, 131);
-            this.lblBrick3.Name = "lblBrick3";
-            this.lblBrick3.Size = new System.Drawing.Size(75, 23);
-            this.lblBrick3.TabIndex = 0;
-            // 
-            // lblBrick4
-            // 
-            this.lblBrick4.BackColor = System.Drawing.Color.Firebrick;
-            this.lblBrick4.Location = new System.Drawing.Point(612, 131);
-            this.lblBrick4.Name = "lblBrick4";
-            this.lblBrick4.Size = new System.Drawing.Size(75, 23);
-            this.lblBrick4.TabIndex = 0;
-            // 
-            // lblBrick7
-            // 
-            this.lblBrick7.BackColor = System.Drawing.Color.Firebrick;
-            this.lblBrick7.Location = new System.Drawing.Point(529, 237);
-            this.lblBrick7.Name = "lblBrick7";
-            this.lblBrick7.Size = new System.Drawing.Size(75, 23);
-            this.lblBrick7.TabIndex = 0;
-            // 
-            // lblBrick6
-            // 
-            this.lblBrick6.BackColor = System.Drawing.Color.Firebrick;
-            this.lblBrick6.Location = new System.Drawing.Point(363, 237);
-            this.lblBrick6.Name = "lblBrick6";
-            this.lblBrick6.Size = new System.Drawing.Size(75, 23);
-            this.lblBrick6.TabIndex = 0;
-            // 
-            // lblBrick5
-            // 
-            this.lblBrick5.BackColor = System.Drawing.Color.Firebrick;
-            this.lblBrick5.Location = new System.Drawing.Point(197, 237);
-            this.lblBrick5.Name = "lblBrick5";
-            this.lblBrick5.Size = new System.Drawing.Size(75, 23);
-            this.lblBrick5.TabIndex = 0;
-            // 
-            // lblBrick8
-            // 
-            this.lblBrick8.BackColor = System.Drawing.Color.Firebrick;
-            this.lblBrick8.Location = new System.Drawing.Point(363, 343);
-            this.lblBrick8.Name = "lblBrick8";
-            this.lblBrick8.Size = new System.Drawing.Size(75, 23);
-            this.lblBrick8.TabIndex = 0;
             // 
             // tmrBall
             // 
-            this.tmrBall.Interval = 2;
+            this.tmrBall.Enabled = true;
+            this.tmrBall.Interval = 7;
             this.tmrBall.Tick += new System.EventHandler(this.tmrBall_Tick);
             // 
-            // pictureBox1
+            // leftWall
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(32, 768);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.leftWall.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("leftWall.BackgroundImage")));
+            this.leftWall.Location = new System.Drawing.Point(0, 0);
+            this.leftWall.Name = "leftWall";
+            this.leftWall.Size = new System.Drawing.Size(32, 768);
+            this.leftWall.TabIndex = 3;
+            this.leftWall.TabStop = false;
             // 
-            // pictureBox2
+            // TopWall
             // 
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(1024, 32);
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.TopWall.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("TopWall.BackgroundImage")));
+            this.TopWall.Location = new System.Drawing.Point(0, 0);
+            this.TopWall.Name = "TopWall";
+            this.TopWall.Size = new System.Drawing.Size(1024, 32);
+            this.TopWall.TabIndex = 3;
+            this.TopWall.TabStop = false;
             // 
-            // pictureBox3
+            // rightWall
             // 
-            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
-            this.pictureBox3.Location = new System.Drawing.Point(992, 0);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(32, 768);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.rightWall.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rightWall.BackgroundImage")));
+            this.rightWall.Location = new System.Drawing.Point(992, 0);
+            this.rightWall.Name = "rightWall";
+            this.rightWall.Size = new System.Drawing.Size(32, 768);
+            this.rightWall.TabIndex = 3;
+            this.rightWall.TabStop = false;
             // 
-            // pictureBox5
+            // paddle
             // 
-            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
-            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox5.Location = new System.Drawing.Point(555, 455);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(96, 24);
-            this.pictureBox5.TabIndex = 5;
-            this.pictureBox5.TabStop = false;
+            this.paddle.BackColor = System.Drawing.Color.Transparent;
+            this.paddle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("paddle.BackgroundImage")));
+            this.paddle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.paddle.Location = new System.Drawing.Point(464, 732);
+            this.paddle.Name = "paddle";
+            this.paddle.Size = new System.Drawing.Size(96, 24);
+            this.paddle.TabIndex = 5;
+            this.paddle.TabStop = false;
             // 
-            // pictureBox4
+            // ball
             // 
-            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
-            this.pictureBox4.Location = new System.Drawing.Point(647, 391);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(30, 30);
-            this.pictureBox4.TabIndex = 6;
-            this.pictureBox4.TabStop = false;
+            this.ball.BackColor = System.Drawing.Color.Transparent;
+            this.ball.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ball.BackgroundImage")));
+            this.ball.Location = new System.Drawing.Point(497, 704);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 6;
+            this.ball.TabStop = false;
             // 
             // frmGame
             // 
@@ -204,21 +104,11 @@
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1024, 768);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.ball);
             this.Controls.Add(this.paddle);
-            this.Controls.Add(this.lblBrick5);
-            this.Controls.Add(this.lblBrick8);
-            this.Controls.Add(this.lblBrick6);
-            this.Controls.Add(this.lblBrick7);
-            this.Controls.Add(this.lblBrick4);
-            this.Controls.Add(this.lblBrick3);
-            this.Controls.Add(this.lblBrick2);
-            this.Controls.Add(this.lblBrick1);
+            this.Controls.Add(this.TopWall);
+            this.Controls.Add(this.rightWall);
+            this.Controls.Add(this.leftWall);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -227,33 +117,22 @@
             this.Text = "Арканоид";
             this.Shown += new System.EventHandler(this.frmGame_Shown);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmGame_KeyDown);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftWall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopWall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightWall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button paddle;
-        private System.Windows.Forms.RadioButton ball;
-        private System.Windows.Forms.Label lblBrick1;
-        private System.Windows.Forms.Label lblBrick2;
-        private System.Windows.Forms.Label lblBrick3;
-        private System.Windows.Forms.Label lblBrick4;
-        private System.Windows.Forms.Label lblBrick7;
-        private System.Windows.Forms.Label lblBrick6;
-        private System.Windows.Forms.Label lblBrick5;
-        private System.Windows.Forms.Label lblBrick8;
         private System.Windows.Forms.Timer tmrBall;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox leftWall;
+        private System.Windows.Forms.PictureBox TopWall;
+        private System.Windows.Forms.PictureBox rightWall;
+        private System.Windows.Forms.PictureBox paddle;
+        private System.Windows.Forms.PictureBox ball;
     }
 }
--- Arkanoid/Arkanoid/frmGame.cs
+++ Arkanoid/Arkanoid/frmGame.cs
@@ -13,10 +13,16 @@ namespace Arkanoid
     public partial class frmGame : Form
     {
         Rectangle gameField;
-        //Rectangle paddle;
-        //Rectangle ball;
+        Rectangle paddleRect;
+        Rectangle ballRect;
         Point ballDirection;
-        int paddleVelocity = 10;
+        int shadowOffset = 4;
+        int paddleVelocity = 15;
+        Point startBallLocation;
+        Point startPaddleLocation;
+        Point startBallDirection;
+        List<PictureBox> bricks;
+        Point startBricks;
 
         public frmGame()
         {
@@ -25,16 +31,33 @@ namespace Arkanoid
 
         private void initGame()
         {
-            gameField = new Rectangle();
-            //paddleX = paddle.Location.X;
-            //paddleY = paddle.Location.Y;
-            //wallLx = lblWallLeft.Location.X + lblWallLeft.Size.Width;
-            //wallRx = lblWallRight.Location.X;
-            //wallTy = lblWallTop.Location.X + lblWallTop.Size.Height;
-            //ballX = ball.Location.X;
-            //ballY = ball.Location.Y;
-            //ballSX = ballShiftX;
-            //ballSY = -ballShiftY;
+            Point lp = new Point(leftWall.Location.X + leftWall.Size.Width, TopWall.Location.Y + TopWall.Size.Height);
+            gameField = new Rectangle(lp.X, lp.Y, rightWall.Location.X - lp.X, this.Size.Height - (TopWall.Location.Y + TopWall.Size.Height));
+            paddleRect = new Rectangle(paddle.Location.X, paddle.Location.Y, paddle.Size.Width - shadowOffset, paddle.Size.Height - shadowOffset);
+            ballRect = new Rectangle(ball.Location.X, ball.Location.Y, ball.Size.Width - shadowOffset, ball.Size.Height - shadowOffset);
+            ballDirection = new Point(3, -3);
+            startBallDirection = ballDirection;
+            startBallLocation = ballRect.Location;
+            startPaddleLocation = paddleRect.Location;
+            fillBricksGameField();
+        }
+
+        private void fillBricksGameField()
+        {
+            startBricks = new Point(40, 40);
+            bricks = new List<PictureBox>();
+            for (int j = 8; j >= 0; j--)
+                for (int i = 12; i >= 0; i--)
+                {
+                    PictureBox pcb = new PictureBox();
+                    pcb.Size = new System.Drawing.Size(76, 36);
+                    pcb.Location = new Point((i * pcb.Size.Width) - (i * shadowOffset) + startBricks.X, (j * pcb.Size.Height) - (j * shadowOffset) + startBricks.Y);
+                    pcb.BackgroundImage = global::Arkanoid.Properties.Resources.brick;
+                    pcb.BackColor = System.Drawing.Color.Transparent;
+                    pcb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+                    this.Controls.Add(pcb);
+                    bricks.Add(pcb);
+                }
         }
 
         private void frmGame_KeyDown(object sender, KeyEventArgs e)
@@ -42,12 +65,12 @@ namespace Arkanoid
             Keys key = e.KeyCode;
             switch (key)
             {
-                //case Keys.Left:
-                //    shiftPaddle(-paddleShiftX);
-                //    break;
-                //case Keys.Right:
-                //    shiftPaddle(paddleShiftX);
-                //    break;
+                case Keys.Left:
+                    shiftPaddle(-paddleVelocity);
+                    break;
+                case Keys.Right:
+                    shiftPaddle(paddleVelocity);
+                    break;
                 case Keys.Enter:
                     tmrBall.Enabled = true;
                     break;
@@ -61,34 +84,74 @@ namespace Arkanoid
 
         private void shiftPaddle(int sx)
         {
-            //int aXLeft = paddleX + sx;
-            //int aXRight = aXLeft + paddle.Size.Width;
-            //if (wallLx > aXLeft) aXLeft = wallLx;
-            //if (wallRx < aXRight) aXLeft = wallRx - paddle.Size.Width;
-            //paddleX = aXLeft;
-            //paddle.Location = new Point(paddleX, paddleY);
+            paddleRect.Location = new Point(paddleRect.Location.X + sx, paddleRect.Location.Y);
+            if (paddleRect.Left < gameField.Left)
+                paddleRect.Location = new Point(gameField.Left, paddleRect.Location.Y);
+            if (paddleRect.Right > gameField.Right)
+                paddleRect.Location = new Point(gameField.Right - paddleRect.Size.Width, paddleRect.Location.Y);
+            paddle.Location = paddleRect.Location;
         }
 
         private void shiftBall()
         {
-            //int bTop, bBot, bLeft, bRight;
-            //bTop = ballX;
-            //bBot = ballX + ball.Size.Width;
-            //bLeft = ballY;
-            //bRight = ballY + ball.Size.Height;
-            //if ((bLeft + ballSX) < wallLx)
-            //    ballSX = ballShiftX;
-            //if (bRight + ballSX > wallRx)
-            //    ballSX = -ballShiftX;
-            //// переписать нахрен
-            //ballX += ballSX;
-            //ballY += ballSY;
-            //ball.Location = new Point(ballX, ballY);
+            if (ballRect.Left < gameField.Left)
+                ballDirection.X = Math.Abs(ballDirection.X);
+            if (ballRect.Right > gameField.Right)
+                ballDirection.X = -(Math.Abs(ballDirection.X));
+            if (ballRect.Top < gameField.Top)
+                ballDirection.Y = Math.Abs(ballDirection.Y);
+
+            if (ballRect.Bottom > paddleRect.Top)
+            {
+                Point ballCenter = new Point(ballRect.Left + (ballRect.Size.Width / 2), ballRect.Top + (ballRect.Size.Height / 2));
+                if (ballCenter.X >= paddleRect.Left & ballCenter.X <= paddleRect.Right)
+                    ballDirection.Y = -(Math.Abs(ballDirection.Y));
+                else if (ballRect.Right >= paddleRect.Left & ballRect.Right <= paddleRect.Right)
+                {
+                    ballDirection.Y = -(Math.Abs(ballDirection.Y));
+                    ballDirection.X = -(Math.Abs(ballDirection.X));
+                }
+                else if (ballRect.Left >= paddleRect.Left & ballRect.Left <= paddleRect.Right)
+                {
+                    ballDirection.Y = -(Math.Abs(ballDirection.Y));
+                    ballDirection.X = Math.Abs(ballDirection.X);
+                }
+            }
+
+            ballRect.Location = new Point(ballRect.Location.X + ballDirection.X, ballRect.Location.Y + ballDirection.Y);
+            if (ballRect.Bottom > gameField.Bottom)
+            {
+                placeToStartLocation();
+                return;
+            }
+            ball.Location = ballRect.Location;
         }
 
+        private void intersects()
+        {
+            Point ballCenter = new Point(ballRect.Left + (ballRect.Size.Width / 2), ballRect.Top + (ballRect.Size.Height / 2));
+            foreach (PictureBox pcb in bricks)
+            {
+                if (pcb.Visible)
+                {
+                    Point pcbCenter = new Point(pcb.Location.X + (pcb.Size.Width / 2), pcb.Location.Y + (pcb.Size.Height / 2));
+                }
+            }
+
+        }
+
+        private void placeToStartLocation()
+        {
+            paddleRect.Location = startPaddleLocation;
+            ballRect.Location = startBallLocation;
+            ballDirection = startBallDirection;
+            paddle.Location = paddleRect.Location;
+            ball.Location = ballRect.Location;
+        }
         private void tmrBall_Tick(object sender, EventArgs e)
         {
             shiftBall();
+            intersects();
         }
     }
 }
