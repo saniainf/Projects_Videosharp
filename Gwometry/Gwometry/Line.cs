2cb86adb4e71ab6bbc581e3ab6b3490b97889bbf
inf <saniainf@gmail.com>
2016-08-04 17:52:18 +0700

    123444

---
+++ Gwometry/Gwometry/Line.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gwometry
+{
+    class Line
+    {
+        public int X1, Y1;
+        public int X2, Y2;
+
+        public Line(int x1, int y1, int x2, int y2)
+        {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+        }
+    }
+}
--- Gwometry/Gwometry/frmMain.cs
+++ Gwometry/Gwometry/frmMain.cs
@@ -12,24 +12,44 @@ namespace Gwometry
 {
     public partial class frmMain : Form
     {
+        Bitmap bmp;
+        Graphics graph;
+        Pen pen;
+
+        Line line1;
+        Line line2;
+
         public frmMain()
         {
             InitializeComponent();
+            Init();
             Draw();
+        }
+
+        private void Init()
+        {
+            bmp = new Bitmap(pcbMain.Width, pcbMain.Height);
+            graph = Graphics.FromImage(bmp);
+            pen = new Pen(Color.Blue, 2f);
+
+            line1 = new Line(100, 100, 250, 10);
+            line2 = new Line(250, 10, 400, 100);
         }
 
         private void Draw()
         {
-            Bitmap bmp = new Bitmap(pcbMain.Width, pcbMain.Height);
-            Graphics graph = Graphics.FromImage(bmp);
-            Pen pen = new Pen(Color.Blue, 2f);
             graph.DrawRectangle(pen, 100, 100, 300, 200);
             graph.DrawRectangle(pen, 200, 150, 100, 100);
             graph.DrawLine(pen, 250, 150, 250, 250);
             graph.DrawLine(pen, 250, 200, 300, 200);
-            graph.DrawLine(pen, 100, 100, 250, 10);
-            graph.DrawLine(pen, 250, 10, 400, 100);
+            Draw(line1);
+            Draw(line2);
             pcbMain.Image = bmp;
+        }
+
+        private void Draw(Line line)
+        {
+            graph.DrawLine(pen, line.X1, line.Y1, line.X2, line.Y2);
         }
     }
 }
