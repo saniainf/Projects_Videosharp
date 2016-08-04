79b41bb6966d9518e0e2df4a60333776108e5832
inf <saniainf@gmail.com>
2016-08-04 12:31:20 +0600

    geometry

---
+++ Gwometry/Gwometry/frmMain.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Gwometry
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pcbMain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbMain
+            // 
+            this.pcbMain.BackColor = System.Drawing.Color.White;
+            this.pcbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pcbMain.Location = new System.Drawing.Point(0, 0);
+            this.pcbMain.Name = "pcbMain";
+            this.pcbMain.Size = new System.Drawing.Size(637, 516);
+            this.pcbMain.TabIndex = 0;
+            this.pcbMain.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 516);
+            this.Controls.Add(this.pcbMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "GraphicEntities";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbMain;
+    }
+}
+
---
+++ Gwometry/Gwometry/frmMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gwometry
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            Draw();
+        }
+
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pcbMain.Width, pcbMain.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Blue, 2f);
+            graph.DrawRectangle(pen, 100, 100, 300, 200);
+            graph.DrawRectangle(pen, 200, 150, 100, 100);
+            graph.DrawLine(pen, 250, 150, 250, 250);
+            graph.DrawLine(pen, 250, 200, 300, 200);
+            graph.DrawLine(pen, 100, 100, 250, 10);
+            graph.DrawLine(pen, 250, 10, 400, 100);
+            pcbMain.Image = bmp;
+        }
+    }
+}
