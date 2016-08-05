77e05dc6035a677e8b412393bed089071c3120a4
inf <saniainf@gmail.com>
2016-08-05 17:48:35 +0700

    gdfsg

---
+++ Gwometry/Gwometry/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gwometry
+{
+    class Box
+    {
+        public int X1, Y1;
+        public int X2, Y2;
+        public int width;
+        public int height;
+
+        public Box(int x1, int y1, int x2, int y2)
+        {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+            width = x2 - x1;
+            height = y2 - y1;
+        }
+    }
+}
--- Gwometry/Gwometry/frmMain.cs
+++ Gwometry/Gwometry/frmMain.cs
@@ -18,6 +18,10 @@ namespace Gwometry
 
         Line line1;
         Line line2;
+        Line line3;
+        Line line4;
+        Box box1;
+        Box box2;
 
         public frmMain()
         {
@@ -34,22 +38,32 @@ namespace Gwometry
 
             line1 = new Line(100, 100, 250, 10);
             line2 = new Line(250, 10, 400, 100);
+            line3 = new Line(250, 150, 250, 250);
+            line4 = new Line(250, 200, 300, 200);
+            box1 = new Box(100, 100, 400, 300);
+            box2 = new Box(200, 150, 300, 250);
         }
 
         private void Draw()
         {
-            graph.DrawRectangle(pen, 100, 100, 300, 200);
-            graph.DrawRectangle(pen, 200, 150, 100, 100);
-            graph.DrawLine(pen, 250, 150, 250, 250);
-            graph.DrawLine(pen, 250, 200, 300, 200);
             Draw(line1);
             Draw(line2);
+            Draw(line3);
+            Draw(line4);
+            Draw(box1);
+            Draw(box2);
+
             pcbMain.Image = bmp;
         }
 
         private void Draw(Line line)
         {
             graph.DrawLine(pen, line.X1, line.Y1, line.X2, line.Y2);
+        }
+
+        private void Draw(Box box)
+        {
+            graph.DrawRectangle(pen, box.X1, box.Y1, box.width, box.height);
         }
     }
 }
